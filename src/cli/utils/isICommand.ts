78487902da41b676158866f72b41df79f34c8e82
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-01-25 15:58:16 +0200

    fix isCommand

--- src/cli/utils/isICommand.ts
+++ src/cli/utils/isICommand.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ICommand } from '../commands/command.interface.js';
 
-export function isCommand(obj: any): obj is { new (): ICommand } {
+export function isCommand(obj: unknown): obj is { new (...args: unknown[]): ICommand } {
   return typeof obj === 'function' && obj.prototype && typeof obj.prototype.execute === 'function';
 }
